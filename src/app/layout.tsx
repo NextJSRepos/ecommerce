8fab626f889681a537c57f020cf58dc7e62d3bdc
sagathiatejas <sagathiyatejas@gmail.com>
2026-03-23 18:25:23 +0530

    page contents changed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,9 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import Header from "@/components/Header";
-import "./globals.css";
 import { Jost } from "next/font/google";
+import "./globals.css";
 
 const jost = Jost({
   variable: "--font-jost",
-  subsets: ["latin"],
-});
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
   subsets: ["latin"],
 });
 
@@ -24,20 +12,14 @@ export const metadata: Metadata = {
   description: "An e-commerce platform for Nike shoes",
 };
 
-export default function RootLayout({
+export default function RootShell({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html
-      lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-    >
-      <body className={`${jost.className} antialiased`}>
-        <Header />
-        <main className="flex-1">{children}</main>
-      </body>
+    <html lang="en">
+      <body className={`${jost.className} antialiased`}>{children}</body>
     </html>
   );
 }
